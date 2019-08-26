e8c7b11df40e49e1ca3f313ab32727aa8a01aed1
qian.ma <qian.ma@example.com>
2019-08-26 10:33:02 +0800

    1

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+    <el-table :data="tableData"  height="100%">
+            <el-table-column prop="date" label="日期" width="140"></el-table-column>
+            <el-table-column prop="name" label="姓名" width="120"></el-table-column>
+            <el-table-column prop="address" label="地址"></el-table-column>
+          </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    const item = {
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄'
+    }
+    return {
+      tableData: Array(50).fill(item)
+    }
+  },
+  methods: {
+    customer () {
+      this.$axios(
+        {
+          method: 'get',
+          url: '/enocloud/common/customer'
+        }
+      ).then(res => {
+        console.log(res.data.data)
+      })
+    }
+  },
+  mounted () {
+    this.customer()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
