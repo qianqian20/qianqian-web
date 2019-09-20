2e94fec02fc9d5d9aa32e3f1862f6bfa7e59a7fa
qian.ma <qian.ma@example.com>
2019-09-20 18:12:46 +0800

    1

--- src/options/project/en-charts.vue
+++ src/options/project/en-charts.vue
@@ -12,14 +12,30 @@ require('echarts/lib/chart/pie')
 require('echarts/lib/component/tooltip')
 require('echarts/lib/component/title')
 
-var myChart = echarts.init(document.getElementById('en-charts'))
-
-myChart.setOption({
-  name: this.name,
-  type: 'pie',
-  radius: '55%',
-  data: this.value
-})
+const optionsInit = type => {
+  let options = {
+    bar: {
+      color: '#c23531',
+      legend: {
+        data: []
+      },
+      tooltip: {
+        formatter: '{a}<br/>{b}:{c}',
+        trigger: 'item'
+      },
+      xAxis: {
+        type: 'category',
+        data: []
+      },
+      yAxis: {
+        type: 'value',
+        data: []
+      },
+      series: [{ type: 'bar', name: '' }]
+    }
+  }
+  return type ? options[type] : {}
+}
 
 export default {
   name: 'en-charts',
@@ -28,13 +44,44 @@ export default {
       type: Array,
       required: true
     },
-    name: {
+    type: {
+      type: String,
+      default: 'bar'
+    },
+    title: {
       type: String,
       required: true
     }
   },
   data () {
-    return {}
+    return {
+      chart: null
+    }
+  },
+  methods: {
+    getOptions () {
+      let type = this.type
+      let title = this.title
+      let name = this.value.map(item => item.name)
+      let value = this.value.map(item => item.value)
+      let options = this.options || optionsInit(type)
+      options.legend.data = name
+      options.series[0].name = title || ''
+      if (type === 'bar') options.xAxis.data = name
+      options.series[0].data = type === 'bar' ? value : this.value
+      return options
+    },
+    init () {
+      let chart = echarts.init(this.$el, this.theme)
+      this.chart = chart
+      this.setOptions()
+    },
+    setOptions () {
+      this.chart.setOption(this.getOptions(), true)
+    }
+  },
+  mounted () {
+    this.init()
   }
 }
 </script>
--- src/views/owner.vue
+++ src/views/owner.vue
@@ -1,25 +1,41 @@
 <template>
     <div>
         <!-- <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree> -->
+        <en-charts :value="Data" title="新增客户"></en-charts>
     </div>
 </template>
 
 <script>
+import enCharts from '@/options/project/en-charts'
 export default {
+  name: 'owner',
+  components: {
+    enCharts
+  },
   data () {
     return {
-      id: ''
+      // id: ''
+      Data: []
     }
   },
   methods: {
     detail () {
+      // this.$axios(
+      //   {
+      //     method: 'GET',
+      //     url: `/common/customer/${this.$route.params.id}`
+      //   }
+      // ).then(res => {
+      //   console.log(res.data.data)
+      // })
       this.$axios(
         {
           method: 'GET',
-          url: `/common/customer/${this.$route.params.id}`
+          url: '/dashboard'
         }
       ).then(res => {
-        console.log(res.data.data)
+        this.Data = res.data.data[0].customerFrom
+        console.log(res.data.data[0].customerFrom)
       })
     }
   },
